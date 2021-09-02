bc2bcbb9e8a2c7f98c3fe5a65f7619ee1c7dbb02
Giant-Penguins <hcimpenguins@gmail.com>
2021-09-02 10:33:29 +0200

    working api

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles.css'
+
+import React, { Component } from "react";
+
+export default class TableRows extends Component {
+state = {
+data: []
+};
+
+componentDidMount() {
+const url = "https://ghibliapi.herokuapp.com/films/";
+
+fetch(url)
+.then(result => result.json())
+.then(result => {
+this.setState({ data: result });
+});
 }
 
-export default MyApp
+render() {
+return (
+  <tbody>
+  <tr>
+    <td>id</td>
+    <td>title</td>
+    <td>director</td>
+    <td>producer</td>
+  </tr>
+      {this.state.data.map((value, key) => (
+    // eslint-disable-next-line react/jsx-key
+    <tr>
+      <td>{key + 1}</td>
+      <td>{value.title}</td>
+      <td>{value.director}</td>
+      <td>{value.producer}</td>
+    </tr>
+  ))}
+</tbody>
+);
+}
+}
